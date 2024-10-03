c47970d0dbe5df5bae025b1f5cf3a809f747dc2e
Drew <andrewshore123@hotmail.com>
2024-10-03 16:28:11 +0100

    Added game to project and refactored keyboard to work with game

--- front-end/src/Component/Keyboard.jsx
+++ front-end/src/Component/Keyboard.jsx
@@ -1,120 +1,102 @@
-import { useState } from "react";
-import "../../src/keyboard.css";
+import { useEffect, useState } from "react";
+import "../keyboard.css";
 
-function Keyboard() {
-  const [pressedKey, setPressedKey] = useState(null);
+function Keyboard({typedLetter, isSpecialKey}) {
 
-  const handleKeyDown = (e) => {
-    console.log(e.key);
-    setPressedKey(e.key.toLowerCase());
-  };
-
-  const handleKeyUp = () => {
-    setTimeout(() => {
-      setPressedKey(null);
-    }, 100);
-  };
 
   return (
     <div className="container">
       <section>
-        <input
-          className="TextBox"
-          type="text"
-          onKeyDown={handleKeyDown}
-          onKeyUp={handleKeyUp}
-          placeholder="Click Here to Start Typing"
-        ></input>
       </section>
       <section className="Keyboard">
         <section className="row">
-          <p className={`keys ${pressedKey === "`" ? `active` : ``}`}>`</p>
-          <p className={`keys ${pressedKey === "1" ? `active` : ``}`}>1</p>
-          <p className={`keys ${pressedKey === "2" ? `active` : ``}`}>2</p>
-          <p className={`keys ${pressedKey === "3" ? `active` : ``}`}>3</p>
-          <p className={`keys ${pressedKey === "4" ? `active` : ``}`}>4</p>
-          <p className={`keys ${pressedKey === "5" ? `active` : ``}`}>5</p>
-          <p className={`keys ${pressedKey === "6" ? `active` : ``}`}>6</p>
-          <p className={`keys ${pressedKey === "7" ? `active` : ``}`}>7</p>
-          <p className={`keys ${pressedKey === "8" ? `active` : ``}`}>8</p>
-          <p className={`keys ${pressedKey === "9" ? `active` : ``}`}>9</p>
-          <p className={`keys ${pressedKey === "0" ? `active` : ``}`}>0</p>
-          <p className={`keys ${pressedKey === "-" ? `active` : ``}`}>-</p>
-          <p className={`keys ${pressedKey === "=" ? `active` : ``}`}>=</p>
+          <p className={`keys ${typedLetter === "`" ? `active` : ``}`}>`</p>
+          <p className={`keys ${typedLetter === "1" ? `active` : ``}`}>1</p>
+          <p className={`keys ${typedLetter === "2" ? `active` : ``}`}>2</p>
+          <p className={`keys ${typedLetter === "3" ? `active` : ``}`}>3</p>
+          <p className={`keys ${typedLetter === "4" ? `active` : ``}`}>4</p>
+          <p className={`keys ${typedLetter === "5" ? `active` : ``}`}>5</p>
+          <p className={`keys ${typedLetter === "6" ? `active` : ``}`}>6</p>
+          <p className={`keys ${typedLetter === "7" ? `active` : ``}`}>7</p>
+          <p className={`keys ${typedLetter === "8" ? `active` : ``}`}>8</p>
+          <p className={`keys ${typedLetter === "9" ? `active` : ``}`}>9</p>
+          <p className={`keys ${typedLetter === "0" ? `active` : ``}`}>0</p>
+          <p className={`keys ${typedLetter === "-" ? `active` : ``}`}>-</p>
+          <p className={`keys ${typedLetter === "=" ? `active` : ``}`}>=</p>
           <p
             className={`Backspace ${
-              pressedKey === "backspace" ? `active` : ``
+              typedLetter === "Backspace" ? `active` : ``
             }`}
           >
             Backspace
           </p>
         </section>
         <section className="row">
-          <p className={`Tab ${pressedKey === "tab" ? `active` : ``}`}>Tab</p>
-          <p className={`keys ${pressedKey === "q" ? `active` : ``}`}>Q</p>
-          <p className={`keys ${pressedKey === "w" ? `active` : ``}`}>W</p>
-          <p className={`keys ${pressedKey === "e" ? `active` : ``}`}>E</p>
-          <p className={`keys ${pressedKey === "r" ? `active` : ``}`}>R</p>
-          <p className={`keys ${pressedKey === "t" ? `active` : ``}`}>T</p>
-          <p className={`keys ${pressedKey === "y" ? `active` : ``}`}>Y</p>
-          <p className={`keys ${pressedKey === "i" ? `active` : ``}`}>I</p>
-          <p className={`keys ${pressedKey === "o" ? `active` : ``}`}>O</p>
-          <p className={`keys ${pressedKey === "p" ? `active` : ``}`}>P</p>
-          <p className={`keys ${pressedKey === "[" ? `active` : ``}`}>{`[`}</p>
-          <p className={`keys ${pressedKey === "]" ? `active` : ``}`}>{`]`}</p>
-          <p className={`Slash ${pressedKey === "\\" ? `active` : ``}`}>\</p>
+          <p className={`Tab ${typedLetter === "Tab" ? `active` : ``}`}>Tab</p>
+          <p className={`keys ${typedLetter === "q" ? `active` : ``}`}>Q</p>
+          <p className={`keys ${typedLetter === "w" ? `active` : ``}`}>W</p>
+          <p className={`keys ${typedLetter === "e" ? `active` : ``}`}>E</p>
+          <p className={`keys ${typedLetter === "r" ? `active` : ``}`}>R</p>
+          <p className={`keys ${typedLetter === "t" ? `active` : ``}`}>T</p>
+          <p className={`keys ${typedLetter === "y" ? `active` : ``}`}>Y</p>
+          <p className={`keys ${typedLetter === "i" ? `active` : ``}`}>I</p>
+          <p className={`keys ${typedLetter === "o" ? `active` : ``}`}>O</p>
+          <p className={`keys ${typedLetter === "p" ? `active` : ``}`}>P</p>
+          <p className={`keys ${typedLetter === "[" ? `active` : ``}`}>{`[`}</p>
+          <p className={`keys ${typedLetter === "]" ? `active` : ``}`}>{`]`}</p>
+          <p className={`Slash ${typedLetter === "\\" ? `active` : ``}`}>\</p>
         </section>
         <section className="row">
           <p
-            className={`Caps_Lock ${pressedKey === "capslock" ? `active` : ``}`}
+            className={`Caps_Lock ${isSpecialKey === "Capslock" ? `active` : ``}`}
           >
             Caps Lock
           </p>
-          <p className={`keys ${pressedKey === "a" ? `active` : ``}`}>A</p>
-          <p className={`keys ${pressedKey === "s" ? `active` : ``}`}>S</p>
-          <p className={`keys ${pressedKey === "d" ? `active` : ``}`}>D</p>
-          <p className={`keys ${pressedKey === "f" ? `active` : ``}`}>F</p>
-          <p className={`keys ${pressedKey === "g" ? `active` : ``}`}>G</p>
-          <p className={`keys ${pressedKey === "h" ? `active` : ``}`}>H</p>
-          <p className={`keys ${pressedKey === "j" ? `active` : ``}`}>J</p>
-          <p className={`keys ${pressedKey === "k" ? `active` : ``}`}>K</p>
-          <p className={`keys ${pressedKey === "l" ? `active` : ``}`}>L</p>
-          <p className={`keys ${pressedKey === ";" ? `active` : ``}`}>;</p>
-          <p className={`keys ${pressedKey === "'" ? `active` : ``}`}>'</p>
-          <p className={`Enter_Key ${pressedKey === "enter" ? `active` : ``}`}>
+          <p className={`keys ${typedLetter === "a" ? `active` : ``}`}>A</p>
+          <p className={`keys ${typedLetter === "s" ? `active` : ``}`}>S</p>
+          <p className={`keys ${typedLetter === "d" ? `active` : ``}`}>D</p>
+          <p className={`keys ${typedLetter === "f" ? `active` : ``}`}>F</p>
+          <p className={`keys ${typedLetter === "g" ? `active` : ``}`}>G</p>
+          <p className={`keys ${typedLetter === "h" ? `active` : ``}`}>H</p>
+          <p className={`keys ${typedLetter === "j" ? `active` : ``}`}>J</p>
+          <p className={`keys ${typedLetter === "k" ? `active` : ``}`}>K</p>
+          <p className={`keys ${typedLetter === "l" ? `active` : ``}`}>L</p>
+          <p className={`keys ${typedLetter === ";" ? `active` : ``}`}>;</p>
+          <p className={`keys ${typedLetter === "'" ? `active` : ``}`}>'</p>
+          <p className={`Enter_Key ${isSpecialKey === "Enter" ? `active` : ``}`}>
             Enter
           </p>
         </section>
         <section className="row">
-          <p className={`Shift_Left ${pressedKey === "shift" ? `active` : ``}`}>
+          <p className={`Shift_Left ${isSpecialKey === "Shift" ? `active` : ``}`}>
             Shift
           </p>
-          <p className={`keys ${pressedKey === "z" ? `active` : ``}`}>Z</p>
-          <p className={`keys ${pressedKey === "x" ? `active` : ``}`}>X</p>
-          <p className={`keys ${pressedKey === "c" ? `active` : ``}`}>C</p>
-          <p className={`keys ${pressedKey === "v" ? `active` : ``}`}>V</p>
-          <p className={`keys ${pressedKey === "b" ? `active` : ``}`}>B</p>
-          <p className={`keys ${pressedKey === "n" ? `active` : ``}`}>N</p>
-          <p className={`keys ${pressedKey === "," ? `active` : ``}`}>,</p>
-          <p className={`keys ${pressedKey === "." ? `active` : ``}`}>.</p>
-          <p className={`keys ${pressedKey === "/" ? `active` : ``}`}>/</p>
-          <p className={`keys ${pressedKey === "?" ? `active` : ``}`}>?</p>
+          <p className={`keys ${typedLetter === "z" ? `active` : ``}`}>Z</p>
+          <p className={`keys ${typedLetter === "x" ? `active` : ``}`}>X</p>
+          <p className={`keys ${typedLetter === "c" ? `active` : ``}`}>C</p>
+          <p className={`keys ${typedLetter === "v" ? `active` : ``}`}>V</p>
+          <p className={`keys ${typedLetter === "b" ? `active` : ``}`}>B</p>
+          <p className={`keys ${typedLetter === "n" ? `active` : ``}`}>N</p>
+          <p className={`keys ${typedLetter === "," ? `active` : ``}`}>,</p>
+          <p className={`keys ${typedLetter === "." ? `active` : ``}`}>.</p>
+          <p className={`keys ${typedLetter === "/" ? `active` : ``}`}>/</p>
+          <p className={`keys ${typedLetter === "?" ? `active` : ``}`}>?</p>
           <p
-            className={`Shift_Right ${pressedKey === "shift" ? `active` : ``}`}
+            className={`Shift_Right ${isSpecialKey === "Shift" ? `active` : ``}`}
           >
             Shift
           </p>
         </section>
         <section className="row">
-          <p className={`Ctrl ${pressedKey === "control" ? `active` : ``}`}>
+          <p className={`Ctrl ${isSpecialKey === "Control" ? `active` : ``}`}>
             Ctrl
           </p>
-          <p className={`Alt ${pressedKey === "alt" ? `active` : ``}`}>Alt</p>
-          <p className={`Space_Key ${pressedKey === " " ? `active` : ``}`}>
+          <p className={`Alt ${isSpecialKey === "Alt" ? `active` : ``}`}>Alt</p>
+          <p className={`Space_Key ${isSpecialKey === " " ? `active` : ``}`}>
             ____
           </p>
-          <p className={`Ctrl ${pressedKey === "alt" ? `active` : ``}`}>Alt</p>
-          <p className={`Alt ${pressedKey === "control" ? `active` : ``}`}>
+          <p className={`Ctrl ${isSpecialKey === "Alt" ? `active` : ``}`}>Alt</p>
+          <p className={`Alt ${isSpecialKey === "Control" ? `active` : ``}`}>
             Ctrl
           </p>
         </section>
