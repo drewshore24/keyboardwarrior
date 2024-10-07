b41809a7e904ab5236d4c1d510cb0306c00a3811
kinus00 <usuncodes@gmail.com>
2024-10-07 09:58:40 +0100

    cleaned up WordGame

--- front-end/src/Component/WordGame.jsx
+++ front-end/src/Component/WordGame.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect, useRef, useContext } from "react";
 import "../WordGame.css";
-import { createData, readData, updateData } from "../utils/crud";
+
 import { UserContext } from "../context/AuthContext";
 import {
   getAverage,
   highScoreCalc,
   noOfGames,
-  updateGameStats,
   updateLastTen,
 } from "../utils/otherUtils";
 import { db } from "../firebase/fire";
@@ -14,27 +13,19 @@ import { doc, updateDoc } from "firebase/firestore";
 const paragraph =
   "As the sun dipped below the horizon, the sky transformed into a canvas of vibrant oranges and deep purples, casting a warm glow over the quiet town. The evening breeze carried the sweet scent of blooming jasmine, mingling with the distant sounds of laughter and music from a nearby festival. Streetlights flickered to life, illuminating the cobblestone streets where families strolled leisurely, savoring the moment. In this tranquil setting, time seemed to slow, allowing the beauty of the world to unfold in every detail.";
 
-const WordGame = ({
-  typedLetter,
-  setTypedLetter,
-  isSpecialKey,
-  setSpecialKey,
-}) => {
-  //   const [typedLetter, setTypedLetter] = useState(null);
+const WordGame = ({ typedLetter, setTypedLetter, setSpecialKey }) => {
   const { user, stats, setStats } = useContext(UserContext);
-
 
   const inputRef = useRef(null);
   const [strArray, setStrArr] = useState([]);
   const [timer, setTimer] = useState(10);
   const [timerStarted, setTimerStarted] = useState(false);
-  const [correctChar, setCorrectChar] = useState(0)
-  const [cpm, setCpm] = useState(0)
-  const [wpm, setWpm] = useState(0)
-  const [accuracy, setAccuracy] = useState(0)
+  const [correctChar, setCorrectChar] = useState(0);
+  const [cpm, setCpm] = useState(0);
+  const [wpm, setWpm] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
   const [gameStarted, setGameStarted] = useState(false);
   const [isTime0, setIsTime0] = useState(false);
-
 
   useEffect(() => {
     if (timer === 0) {
@@ -97,46 +88,44 @@ const WordGame = ({
     }
   }
   function handleClick() {
-    if(!gameStarted){
-        setGameStarted(true)
+    if (!gameStarted) {
+      setGameStarted(true);
     }
     inputRef.current.focus();
   }
 
-
-  function conditionalRender(){
-    if(gameStarted && timer > 0){
-        return(
-        paragraph.split("").map((char, i) => (
-            <span className={`char ${getClassName(i)}`} key={i}>
-              {char}
-            </span>
-        )
-    ))}
-    if(!gameStarted && timer > 0){
-        return(
-        <span className="start-game-text">Please click here to start the game. Once you start typing, the timer will start...</span>
-        )
+  function conditionalRender() {
+    if (gameStarted && timer > 0) {
+      return paragraph.split("").map((char, i) => (
+        <span className={`char ${getClassName(i)}`} key={i}>
+          {char}
+        </span>
+      ));
     }
-    if(gameStarted && timer === 0){
-        return(
-        <p className="stats-return">Stats</p>
-        )
+    if (!gameStarted && timer > 0) {
+      return (
+        <span className="start-game-text">
+          Please click here to start the game. Once you start typing, the timer
+          will start...
+        </span>
+      );
+    }
+    if (gameStarted && timer === 0) {
+      return <p className="stats-return">Stats</p>;
     }
   }
 
-  function refresh(){
-    setTypedLetter(null)
-    setStrArr([])
-    setTimer(30)
-    setTimerStarted(false)
-    setCorrectChar(0)
-    setCpm(0)
-    setWpm(0)
-    setAccuracy(0)
-    setGameStarted(false)
+  function refresh() {
+    setTypedLetter(null);
+    setStrArr([]);
+    setTimer(30);
+    setTimerStarted(false);
+    setCorrectChar(0);
+    setCpm(0);
+    setWpm(0);
+    setAccuracy(0);
+    setGameStarted(false);
     setIsTime0(false);
-
   }
 
   const userData = {
@@ -192,9 +181,7 @@ const WordGame = ({
             onKeyDown={handleKeyDown}
             ref={inputRef}
           />
-          <div className="text-field">
-            {conditionalRender()}
-          </div>
+          <div className="text-field">{conditionalRender()}</div>
         </div>
       </div>
       <div className="result">
