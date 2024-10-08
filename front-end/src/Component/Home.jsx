b4d135627e238b1e9a9a200879ae5d2cf4f43448
iadam2000 <ibby200@hotmail.co.uk>
2024-10-08 06:21:15 +0300

    Styling changes

--- front-end/src/Component/Home.jsx
+++ front-end/src/Component/Home.jsx
@@ -2,21 +2,30 @@ import { useContext, useState } from "react";
 import { UserContext } from "../context/AuthContext";
 import NavBar from "./NavBar";
 import WordGame from "./WordGame";
-import Login from './Login';
-import Signup from "./SignUp"
+import Login from "./Login";
+import Signup from "./SignUp";
 
 function Home() {
   const { isLoggedOut, user } = useContext(UserContext);
   const [typedLetter, setTypedLetter] = useState(null);
-  const [isSpecialKey, setSpecialKey] = useState(null)
+  const [isSpecialKey, setSpecialKey] = useState(null);
 
   return (
     <>
-    <div><NavBar /></div>
-      <h1>Le Keyboard Warrior</h1>
-      {isLoggedOut ? <Login/> : <h2>Hello {user?.userName} </h2>}
+      <div>
+        <NavBar />
+      </div>
+      <div className="header">
+        <h1>Le Keyboard Warrior</h1>
+      </div>
+      {isLoggedOut ? <Login /> : <h2>Hello {user?.userName} </h2>}
       <div className="Game-Keyboard-Container">
-      <WordGame typedLetter={typedLetter} setTypedLetter={setTypedLetter} isSpecialKey={isSpecialKey} setSpecialKey={setSpecialKey}/>
+        <WordGame
+          typedLetter={typedLetter}
+          setTypedLetter={setTypedLetter}
+          isSpecialKey={isSpecialKey}
+          setSpecialKey={setSpecialKey}
+        />
       </div>
     </>
   );
