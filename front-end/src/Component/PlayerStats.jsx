6fedf8376dda8b1f38af39cbdf6cf5a10da116f2
iadam2000 <ibby200@hotmail.co.uk>
2024-10-09 06:03:35 +0300

    Styling mostly finished, some finishing touches required still

--- front-end/src/Component/PlayerStats.jsx
+++ front-end/src/Component/PlayerStats.jsx
@@ -30,39 +30,49 @@ const PlayerStats = () => {
 
   return (
     <div className="player-stats-card">
+
       <div className="header">
         <h3 className="stats-header">{user?.userName}'s Stats</h3>
       </div>
+
       <div className="content">
+
         <dl className="stats-details">
           <div className="stat">
             <dt>Games played</dt>
             <dd>{stats?.gamesPlayed}</dd>
           </div>
+
           <div className="stat">
             <dt>Highest Score</dt>
             <dd>{stats?.highScore}</dd>
           </div>
+
           <div className="stat">
             <dt>Words Per Minute (latest game)</dt>
             <dd>{stats?.wpm}</dd>
           </div>
+
           <div className="stat">
             <dt>Characters Per Minute (latest game)</dt>
             <dd>{stats?.cpm}</dd>
           </div>
+
           <div className="stat">
             <dt>Accuracy (latest game)</dt>
             <dd>{stats?.accuracy}%</dd>
           </div>
+
           <div className="stat">
             <dt>Average Accuracy (last ten games)</dt>
             <dd>{stats?.averageAccuracy}%</dd>
           </div>
+
           <div className="stat">
             <dt>Average Words Per Minute (last ten games)</dt>
             <dd>{stats?.averageWpm}</dd>
           </div>
+          
         </dl>
       </div>
     </div>
